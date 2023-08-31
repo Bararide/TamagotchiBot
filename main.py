eec616e29e96bd88205e3670505870a3d66ad3fb
Bararide <118819233+Bararide@users.noreply.github.com>
2023-09-01 01:02:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import aiogram
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-API_KEY = ('6451676145:AAFcWrDgRersroao4CPTUdB8Sno2PSN72Po')
+API_KEY = ('YOUR_API_KEY')
 bot = aiogram.Bot(API_KEY)
-storage = MemoryStorage()
 dp = aiogram.Dispatcher(bot, storage=storage)
