e700f852c2362994fc9ef42ff2f3bd76076a2e4d
Bararide <118819233+Bararide@users.noreply.github.com>
2023-09-22 10:10:02 +0300

    Update pet_container.py

--- pet_container.py
+++ pet_container.py
@@ -1,15 +1,1 @@
-class Pet:
-    def __init__(self, owner, animal, pet_name, latitude = None, longitude = None, health = 100) -> None:
-        self.pet_name = pet_name
-        self.animal = animal
-        self.health = health
-        self.owner = owner
-        self.latitude = latitude
-        self.longitude = longitude
 
-    def __init__(self) -> None:
-        self.latitude = None
-        self.longitude = None
-
-    def return_pet_list(self) -> list:
-        return [self.animal, self.pet_name]
